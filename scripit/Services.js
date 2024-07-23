b93ec50583b40d726291c8868fb03238300597bb
gabrielpik <114487101+gabrielpik@users.noreply.github.com>
2024-07-22 22:08:36 -0300

    testando hipotese de todos os fetch

--- scripit/Services.js
+++ scripit/Services.js
@@ -31,7 +31,7 @@ export default class Services{
             
         }
 
-await fetch(`${this.servidor}/arvore`, {
+await fetch(`${this.servidor}arvore`, {
     method: "GET",
     headers: {
         'ngrok-skip-browser-warning': 'true',
@@ -188,7 +188,7 @@ async Path(arrow){
   
 
 
-    await fetch("https://7c9e-177-191-113-88.ngrok-free.app",
+    await fetch(this.servidor+"/arvore",
         {
 
         headers: {
